40a08235e0d8da8ef2e5456c6cae18648046ee09
PrzemekCzajka <87855707+PrzemekCzajka@users.noreply.github.com>
2022-09-26 10:05:05 +0200

    del code

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-my_number = 1
-while my_number % 6 == 0:
-    print(my_number)
-    if my_number == 180:
-        break
-    my_number+=1
-    
+
     print("'Hiszpańska Inkwizycja' to najlepszy skecz grupy Monty Pythona")
